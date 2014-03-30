6b071a89fd855eee59761448718a9fea4f7e29cd
Max Ott <max.ott@nicta.com.au>
2014-03-31 08:49:07 +1100

    Adding preliminary support to create and use iBook widgets. Kind of working (can create experiments), but graphs aren't working, yet due to issues with session ID. Not clear if websockets are working at all.

---
+++ iwidget/config.js
@@ -0,0 +1,16 @@
+
+WidgetConfig = {
+  url: 'http://localhost:4000',
+  path: '/_column',
+  type: 'GET',
+  opts: {
+    col: 'execute',
+    action: 'get_content',
+    url: "git:default:repo/oidl/simple_oml_test.rb",
+    mime_type: "text/ruby",
+    iwidget: {
+      width: 600,
+      height: 800
+    }
+  }
+};
