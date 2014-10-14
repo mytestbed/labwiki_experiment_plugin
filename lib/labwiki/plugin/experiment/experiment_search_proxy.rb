cb021fb1c135f6a13af43147c0cbe0036e2cf850
Max Ott <max.ott@nicta.com.au>
2014-10-14 13:06:15 +1100

    changed experiment search method according to new async search scheme - not tested!

--- lib/labwiki/plugin/experiment/experiment_search_proxy.rb
+++ lib/labwiki/plugin/experiment/experiment_search_proxy.rb
@@ -12,47 +12,50 @@ module LabWiki::Plugin::Experiment
       proxy
     end
 
-    def find(pattern, opts, wopts)
+    def find(pattern, opts, wopts, &cbk)
       unless @url
         js = wopts[:job_service]
         @url = "http://#{js[:host]}:#{js[:port] || 80}/jobs?"
       end
 
       opts[:mime_type] = 'text/ruby'
-      files = OMF::Web::ContentRepository.find_files(pattern, opts)
+      OMF::Web::ContentRepository.find_files(pattern, opts, &cbk)
 
-      if @error_at
-        if (Time.now - @error_at) > 60 # try again
-          @error_at = nil
-        else
-          return files
-        end
-      end
+      query(pattern, OMF::Web::SessionStore[:id, :user], &cbk)
 
-      #puts "FIND: '#{pattern}' - opts: #{opts}"
 
-      if result = @results[pattern]
-        if (Time.now - result[:time]) > 30
-          # refresh
-          @results.delete(pattern)
-          result = nil
-        end
-      end
+      # if @error_at
+      #   if (Time.now - @error_at) > 60 # try again
+      #     @error_at = nil
+      #   else
+      #     return files
+      #   end
+      # end
 
-      unless result
-        query(pattern, OMF::Web::SessionStore[:id, :user])
-        raise LabWiki::RetrySearchLaterException.new
-      end
-      experiments = result[:jobs].map do |r|
-        {
-          url: r["href"], name: r["name"], status: r['status'],
-          mime_type: 'experiment', widget: 'experiment' #, plugin: 'experiment'
-        }
-      end
-      experiments.concat(files)
+      # #puts "FIND: '#{pattern}' - opts: #{opts}"
+
+      # if result = @results[pattern]
+      #   if (Time.now - result[:time]) > 30
+      #     # refresh
+      #     @results.delete(pattern)
+      #     result = nil
+      #   end
+      # end
+
+      # unless result
+      #   query(pattern, OMF::Web::SessionStore[:id, :user])
+      #   raise LabWiki::RetrySearchLaterException.new
+      # end
+      # experiments = result[:jobs].map do |r|
+      #   {
+      #     url: r["href"], name: r["name"], status: r['status'],
+      #     mime_type: 'experiment', widget: 'experiment' #, plugin: 'experiment'
+      #   }
+      # end
+      # experiments.concat(files)
     end
 
-    def query(pat, username)
+    def query(pat, username, &cbk)
       EventMachine.synchrony do
         begin
           resp = EventMachine::HttpRequest.new(@url).get(query: {pat: pat, username: username})
@@ -63,7 +66,13 @@ module LabWiki::Plugin::Experiment
             warn "Job search failed #{resp.response} - #{resp.inspect}"
           else
             reply = JSON.parse(resp.response)
-            @results[pat] = {time: Time.now, jobs: reply}
+            reply.each do |r|
+              cbk.call({
+                url: r["href"], name: r["name"], status: r['status'],
+                mime_type: 'experiment', widget: 'experiment' #, plugin: 'experiment'
+              })
+            end
+            #@results[pat] = {time: Time.now, jobs: reply}
             #p reply
           end
 
